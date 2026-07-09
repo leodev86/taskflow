fd05b7df81b1cb7e52d71d318b1cdb293d5c75a8
antonio leon <leonantoniodev@gmail.com>
2026-07-09 01:23:19 -0500

    Integrar localStorage para persistir tareas

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,85 +1,51 @@
-import { useState } from "react";
+import TaskForm from "../components/TaskForm";
 import TaskList from "../components/TaskList";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import type { Task } from "../types/task";
 
-
 export default function Dashboard() {
+  const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", []);
 
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [title, setTitle] = useState("");
-
-
-  function addTask(){
-
-    if(!title.trim()) return;
-
+  function addTask(title: string) {
     const newTask: Task = {
       id: crypto.randomUUID(),
       title,
-      completed:false
+      completed: false,
     };
-
 
     setTasks([
       ...tasks,
-      newTask
+      newTask,
     ]);
-
-    setTitle("");
   }
 
-
-  function toggleTask(id:string){
-
+  function toggleTask(id: string) {
     setTasks(
-      tasks.map(task =>
+      tasks.map((task) =>
         task.id === id
-        ? {...task, completed: !task.completed}
-        : task
+          ? {
+              ...task,
+              completed: !task.completed,
+            }
+          : task
       )
     );
-
   }
 
-
-  function deleteTask(id:string){
-
+  function deleteTask(id: string) {
     setTasks(
-      tasks.filter(task => task.id !== id)
+      tasks.filter((task) => task.id !== id)
     );
-
   }
-
-
 
   return (
-
     <main className="min-h-screen bg-slate-900 p-8">
 
       <h1 className="text-4xl text-cyan-400 font-bold mb-8">
         TaskFlow 🚀
       </h1>
 
-
-      <div className="flex gap-3 mb-8">
-
-        <input
-          className="p-3 rounded bg-white"
-          value={title}
-          onChange={(e)=>setTitle(e.target.value)}
-          placeholder="Nueva tarea..."
-        />
-
-
-        <button
-          onClick={addTask}
-          className="bg-cyan-500 px-5 rounded"
-        >
-          Agregar
-        </button>
-
-      </div>
-
+      <TaskForm addTask={addTask} />
 
       <TaskList
         tasks={tasks}
@@ -87,8 +53,6 @@ export default function Dashboard() {
         deleteTask={deleteTask}
       />
 
-
     </main>
-
   );
 }
